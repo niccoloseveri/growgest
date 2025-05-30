882a70d0d3fde7073ea3c25ac0f888d0f446258e
niccoloseveri <niccolo.severi@libero.it>
2025-05-30 10:43:55 +0200

    First

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
+/*Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
@@ -20,5 +20,5 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 });
-
+*/
 require __DIR__.'/auth.php';
